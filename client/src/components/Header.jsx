9fff1e260ea132f97e6c04527098e217fa4533c0
PavelKrastev-hub <pavel__krustev@abv.bg>
2025-11-17 15:50:56 +0200

    Extract main components

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,21 @@
+import { Link } from "react-router";
+
 export default function Header() {
     return (
         <header>
             {/* <!-- Navigation --> */}
             <nav>
-                <a className="home" href="#"> <img src="./images/logo.png" alt="logo" /> </a>
-                <a href="#">Catalog</a>
+                <Link className="home" to="/"> <img src="./images/logo.png" alt="logo" /> </Link>
+                <Link to="/games">Catalog</Link>
                 {/* <!-- Logged-in users --> */}
                 <div id="user">
-                    <a href="#">Add Game</a>
-                    <a href="#">Logout</a>
+                    <Link to="/create">Add Game</Link>
+                    <Link to="/logout">Logout</Link>
                 </div>
                 {/* <!-- Guest users --> */}
                 <div id="guest">
-                    <a href="#">Login</a>
-                    <a href="#">Register</a>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
                 </div>
             </nav>
         </header>
