3d8bd1767d5bbddd308df56d7b0595a118877dde
PavelKrastev-hub <pavel__krustev@abv.bg>
2025-12-12 14:37:05 +0200

    Render login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./components/home/Home.jsx"
 import Catalog from "./components/catalog/Catalog.jsx"
 import Details from "./components/details/Details.jsx"
 import GameCreate from "./components/game-create/GameCreate.jsx"
+import Login from "./components/login/Login.jsx"
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
                 <Route path="/games" element={<Catalog />} />
                 <Route path="/games/:gameId/details" element={<Details />} />
                 <Route path="/games/create" element={<GameCreate />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
 
             <Footer />
