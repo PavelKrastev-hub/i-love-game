0b565354d171658eadde71ec87d6d7c4d9281654
PavelKrastev-hub <pavel__krustev@abv.bg>
2025-12-12 15:03:54 +0200

    Login with controlled form

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,14 +1,42 @@
+import { useState } from "react";
+import request from "../../utils/request.js";
+import { useNavigate } from "react-router";
+
 export default function Login() {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const emailChangeHandler = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const passwordChangeHandler = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        try {
+            request('http://localhost:3030/users/login', 'POST', { email, password });
+
+            navigate('/');
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+
     return (
         <section id="login-page">
-            <form id="login">
+            <form id="login" onSubmit={submitHandler}>
                 <div className="container">
                     <h1>Login</h1>
                     <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" placeholder="Your Email" />
+                    <input type="email" value={email} onChange={emailChangeHandler} id="email" name="email" placeholder="Your Email" />
 
                     <label htmlFor="login-pass">Password</label>
-                    <input type="password" id="login-password" name="password" placeholder="Password" />
+                    <input type="password" value={password} onChange={passwordChangeHandler} id="login-password" name="password" placeholder="Password" />
                     <input type="submit" className="btn submit" value="Login" />
                 </div>
             </form >
